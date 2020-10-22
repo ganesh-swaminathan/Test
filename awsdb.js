38485a703034d38522913478eca45cf318c3d94b
vembuganesh-s <Vembuganesh.Swaminathan@mheducation.com>
2020-10-22 12:34:49 +0530

    upadte

--- awsdb.js
+++ awsdb.js
@@ -1,7 +1,7 @@
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://devops:QWEasd123@cluster0.iszc2.mongodb.net/devops?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
