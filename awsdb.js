1ab9131dddb285c156aa54fbf68dd8fe72f85a97
vembuganesh-s <Vembuganesh.Swaminathan@mheducation.com>
2020-10-22 12:39:26 +0530

    we

--- awsdb.js
+++ awsdb.js
@@ -1,5 +1,5 @@
 
-const MongoClient = require('mongodb').MongoClient;
+/*const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://devops:QWEasd123@cluster0.iszc2.mongodb.net/devops?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 client.connect(err => {
@@ -7,3 +7,39 @@ client.connect(err => {
   // perform actions on the collection object
   client.close();
 });
+*/
+
+const {MongoClient} = require('mongodb');
+
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://devops:QWEasd123@cluster0.iszc2.mongodb.net/devops?retryWrites=true&w=majority";
+ 
+
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
